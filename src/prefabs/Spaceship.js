e794a07f34f3222d1d427608ce0e0c5cc37e9197
Danny C <dchan27@ucsc.edu>
2023-04-19 00:56:32 -0700

    explosion follows diagram currently

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -10,7 +10,7 @@ class Spaceship extends Phaser.GameObjects.Sprite
 
     update()
     {
-        this.x -=this.moveSpeed;
+        this.x -= this.moveSpeed;
         if(this.x <= 0 - this.width)
         {
             this.reset;
