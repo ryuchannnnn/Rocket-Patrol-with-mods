39d045f4020591860486688ed3a4ce8f1f8ee339
Danny C <dchan27@ucsc.edu>
2023-04-19 20:53:47 -0700

    restarted and typing

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-let config = {
+let config = 
+{
     type: Phaser.AUTO,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
-  }
+    scene: [Menu,Play]
+}
 
 let game = new Phaser.Game(config);
 
-// set UI sizes
-let borderUISize = game.config.height / 15;
-let borderPadding = borderUISize / 3;
+// reserve keyboard vars 
+let keyF,keyR,keyLEFT, keyRIGHT;
 
-// reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
+// set UI sizes 
+let borderUISize = game.config.height/15;
+let borderPadding = borderUISize / 3;
--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,17 +1,20 @@
-// Rocket prefab
-class Rocket extends Phaser.GameObjects.Sprite 
+// rocket prefab 
+
+class Rocket extends Phaser.GameObjects.Sprite
 {
-    constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
-  
-      // add object to existing scene
-      scene.add.existing(this);
-      this.isFiring = false;
-      this.moveSpeed = 2;
-      this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
+    constructor(scene,x,y,texture,frame)
+    {
+        super(scene,x,y,texture,frame);
+
+        // add object to existing scene
+        scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
     }
 
-    update(){
+    update()
+    {
+        // left and right movement 
         if(!this.isFiring)
         {
             if(keyLEFT.isDown && this.x >= borderUISize + this.width)
@@ -24,26 +27,23 @@ class Rocket extends Phaser.GameObjects.Sprite
             }
         }
 
-        //  fire button
-        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+        // fire button 
+        if(Phaser.Input.Keyboard.JustDown(keyF))
+        {
             this.isFiring = true;
-            this.sfxRocket.play();  // play sfx
         }
+
         // if fired, move up 
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding)
         {
             this.y -= this.moveSpeed;
         }
-        // reset on miss
+
+        // reset on miss 
         if(this.y <= borderUISize * 3 + borderPadding)
         {
-            this.reset;
+            this.isFiring = false;
+            this.y = game.config.height - borderUISize - borderPadding;
         }
     }
-
-    reset()
-    {
-        this.isFiring = false;
-        this.y = game.config.height - borderUISize - borderPadding;
-    }
-  }
+}
