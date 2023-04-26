ab31959e9221db272be1d217ee3da844174733b2
Danny C <dchan27@ucsc.edu>
2023-04-26 05:08:15 -0700

    made comments

--- src/main.js
+++ src/main.js
@@ -10,19 +10,30 @@
 
 // mods I chose(done in order)
 /*
-    Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15): My artwork was drawn on piskel because aseprite costs money
+    1.
+    Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15): 
+    
+    Explanation:
+    My artwork was drawn on piskel because aseprite costs money
     I was able to implement this by making a new Spaceship type, but just making it faster by a little bit.
 
-    Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5): Background music via https://www.FesliyanStudios.com
+    2.
+    Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5): 
+    
+    Explanation:
+    I chose my Background music via https://www.FesliyanStudios.com, their audio is permitted for video games as long as I do not upload to youtube, and citing just requires to put their website.
+    I am not gaining ANY profit off this but I will still cite where I got the audio from. (as well as their policy) https://www.fesliyanstudios.com/policy
+    see citations for how I implemented this **1
 */
 
 // citations:
 /*
+    **1 
     I used this for audio: Background music via https://www.FesliyanStudios.com
     I followed this guide for adding copyright free background music
     https://www.youtube.com/watch?v=COncYQLGJS8&ab_channel=LuisZuno 
 
-    
+
 */
 
 let config = 
